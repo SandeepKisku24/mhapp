f02d2c7a4ce95877c5ba87ee3bd6c3f1bdb04def
SandeepKisku24 <sanjoshkisku24@gmail.com>
2024-06-02 22:21:00 +0530

    changes in slides

--- src/slides.js
+++ src/slides.js
@@ -98,7 +98,7 @@ const Slide = ()=>{
                 </Card.Section>
 
                 <Stack gap={0} pb={5}  style={{
-                    backgroundColor:"#4285F4",
+                    backgroundColor:"#34A853",
                     position:"absolute",
                     top:"410px",
                     width:"90%",
@@ -132,7 +132,7 @@ const Slide = ()=>{
                 </Card.Section>
 
                 <Stack gap={0} pb={5}  style={{
-                    backgroundColor:"#4285F4",
+                    backgroundColor:"#34A853",
                     position:"absolute",
                     top:"410px",
                     width:"90%",
