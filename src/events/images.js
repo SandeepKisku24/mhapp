a508ab13cb7751ef63e289ca6304d93ab0d2f538
SandeepKisku24 <sanjoshkisku24@gmail.com>
2024-06-08 10:24:57 +0530

    changes in events section

--- src/events/images.js
+++ src/events/images.js
@@ -1,5 +1,5 @@
 import { Carousel } from '@mantine/carousel';
-import { Card, Image, Text, Overlay, Group, SimpleGrid } from '@mantine/core';
+import { Card, Image, Text, Overlay, Group, SimpleGrid, Title } from '@mantine/core';
 import '@mantine/carousel/styles.css';
 import "./images.css";
 import { slides } from './data';
@@ -13,12 +13,11 @@ const Images= ({data,onClick})=> {
   return (
     <div className='gallery' style={{width : "90%", margin:"auto"}}>
     
-      <div className="headingEvents">
+      <Title fz="h1" className="headingEvents">
         Monthly Events in Mercy Hospital
-      </div>
+      </Title>
     
     <Carousel
-    withIndicators
     height={300}
 
     slideSize={{ base: '100%', sm: '50%', md: '33.333333%' }}
@@ -34,14 +33,11 @@ const Images= ({data,onClick})=> {
         <div className='galImg'>
         <Image style={{height:"", overflow:"hidden"}} src={slide.src} alt={slide.src} />
         </div>
-        {/* <Overlay 
-        // gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.3))"
-        opacity={1} zIndex={1} blur={0} />  */}
-        {/* <Group position="center" style={{ left: '50%', transform: 'translate(-50%, -50%)', zIndex: 2 }}>  */}
+        
         </Card>
 
         <Group className='gal' justify='center' style={{ backgroundColor:"#fff",padding:"1%",borderRadius:"5px", zIndex: 5 }}> 
-        <Text c="#000" weight={700} size="lg">{slide.title}</Text> 
+        <Text c="grey" weight={700} size="lg" bg="#E8E8E8" px={30} my={5} style={{borderRadius:"5px"}}>{slide.title}</Text> 
         </Group>
       </Carousel.Slide>
     ))
