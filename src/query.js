71e566fe2f25ca9bc40ca2e0f3b5c371873ab7f7
SandeepKisku24 <sanjoshkisku24@gmail.com>
2024-06-25 20:43:50 +0530

    Fixed the Query

--- src/query.js
+++ src/query.js
@@ -2,12 +2,13 @@ import { useState } from 'react';
 import { useForm } from '@mantine/form';
 import { TextInput, Button, Code, Title, Stack } from '@mantine/core';
 import React from 'react';
+import axios from 'axios';
 
-const Query= ()=>{
-    const [submittedValues, setSubmittedValues] = useState('');
+const Query = () => {
+  const [submittedValues, setSubmittedValues] = useState('');
+  const [formStatus, setFormStatus] = useState('');
 
   const form = useForm({
-    mode: 'uncontrolled',
     initialValues: {
       firstName: 'Jane',
       lastName: 'Doe',
@@ -20,12 +21,26 @@ const Query= ()=>{
     }),
   });
 
+  const handleSubmit = async (values) => {
+    setSubmittedValues(JSON.stringify(values, null, 2));
+
+    try {
+      const response = await axios.post('https://formspree.io/f/mpwaaonl', values);
+      if (response.status === 200) {
+        setFormStatus('Form submitted successfully!');
+      } else {
+        setFormStatus('Failed to submit the form.');
+      }
+    } catch (error) {
+      setFormStatus('Failed to submit the form.');
+      console.error('Error submitting form:', error);
+    }
+  };
+
   return (
     <Stack>
-        <Title> Let us know your query </Title>
-      <form
-        onSubmit={form.onSubmit((values) => setSubmittedValues(JSON.stringify(values, null, 2)))}
-        action="mailto:mercyhospital@gmail.com" method="post" >
+      <Title>Let us know your query</Title>
+      <form onSubmit={form.onSubmit(handleSubmit)}>
         <TextInput
           label="First name"
           placeholder="First name"
@@ -54,10 +69,10 @@ const Query= ()=>{
           Submit
         </Button>
       </form>
-
-      
+      {/* {submittedValues && <Code>{submittedValues}</Code>} */}
+      {formStatus && <p>{formStatus}</p>}
     </Stack>
   );
-}
+};
 
 export default Query;
