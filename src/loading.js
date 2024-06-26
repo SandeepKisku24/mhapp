7fd6a5811186f05af33096267168089a72b7f036
SandeepKisku24 <sanjoshkisku24@gmail.com>
2024-06-26 19:35:57 +0530

    dimensions of load

--- src/loading.js
+++ src/loading.js
@@ -2,11 +2,15 @@
 import React from 'react';
 import './loading.css';
 import load from "./gallery/loading.gif"
+import { useMediaQuery } from '@mantine/hooks';
+import { mobile } from './screenSizes';
+import { Image } from '@mantine/core';
 
 const Loading = () => {
+  const isMobile = useMediaQuery(mobile);
   return (
     <div className="loading">
-      <img src={load} alt="Loading..." />
+      <Image w={isMobile?"50":"80"} h={isMobile?"50":"80"} src={load} alt="Loading..." />
     </div>
   );
 };
